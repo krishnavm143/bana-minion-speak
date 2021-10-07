29c8342578e07c27f7013b9ac29bf0cd25c20008
krishna <krishnavm143@gmail.com>
2021-10-08 00:18:24 +0530

    taking input from textare and clicking

---
+++ banana.js
@@ -0,0 +1,11 @@
+var btnTranslate = document.querySelector('#btn-translate')
+var textAreInput=document.querySelector("#text-area-input")
+// console.log(btnTranslate)
+function btnClick(){
+    var click="clicked";
+  console.log("clicked");
+  console.log(textAreInput.value)
+}
+btnTranslate.addEventListener('click',btnClick)
+
+// header.addEventListener()
