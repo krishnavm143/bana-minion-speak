67c49032e45bf399ac0a9a80c1602d19e42a47ea
krishna <krishnavm143@gmail.com>
2021-10-08 12:00:32 +0530

    input selector done

--- banana.js
+++ banana.js
@@ -1,10 +1,13 @@
 var btnTranslate = document.querySelector('#btn-translate')
-var textAreInput=document.querySelector("#text-area-input")
+var textAreInput=document.querySelector("[id='text-area-input']")
+var bringText=document.querySelector("bring-text");
 // console.log(btnTranslate)
 function btnClick(){
     var click="clicked";
   console.log("clicked");
   console.log(textAreInput.value)
+  // bringText.=textAreInput.value;
+  // bringText.innerHTML=textAreInput.value
 }
 btnTranslate.addEventListener('click',btnClick)
 
